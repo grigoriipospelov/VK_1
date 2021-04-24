a01f24e32c2324ef18a456b12f3ef6806d51d810
Grigorii pospelov <newzelend@icloud.com>
2021-04-24 09:13:00 +0300

    Initial Commit

---
+++ VK_1/LoginFormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginFormViewController.swift
+//  VK_1
+//
+//  Created by Grisha Pospelov on 24.04.2021.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
